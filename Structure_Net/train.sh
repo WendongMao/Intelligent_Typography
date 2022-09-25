48c242efc808c720adb26b250f471fd00c388edf
WendongMao <48204105+WendongMao@users.noreply.github.com>
2022-09-25 22:33:30 +0800

    Update train.sh

--- Structure_Net/train.sh
+++ Structure_Net/train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python style.py train --style-image "./style_imgs/binggan.jpg" --dataset "/media/HD1/Datasets/coco2014/" --gpu 1 --visualize 1
+CUDA_VISIBLE_DEVICES=3 python style.py train --style-image "./style_imgs/leaf2.jpg" --dataset "/media/HD1/Datasets/coco2014/" --gpu 1 --visualize 1
