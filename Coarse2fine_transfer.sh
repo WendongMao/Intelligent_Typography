a6d94220957e18d7808306dc407bd30e62c8c071
WendongMao <48204105+WendongMao@users.noreply.github.com>
2022-09-27 10:47:39 +0800

    Update Coarse2fine_transfer.sh

--- Coarse2fine_transfer.sh
+++ Coarse2fine_transfer.sh
@@ -1,3 +1,3 @@
 python Forward_pro_gen.py 
-python ./Structure_Net/style.py transfer --model-path "./Structure_Net/models/leaf.model" --source "./Gp1_prototype.jpg" --output "./Ns_result.jpg" 
+python ./Structure_Net/style.py transfer --model-path "./Structure_Net/models/ink.model" --source "./Gp1_prototype.jpg" --output "./Ns_result.jpg" 
 python ./Texture_Net/texture_refine.py
